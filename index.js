4ece6de1c5d0e755deb7a7d93006ca36a6deae9a
unknown <timesetday61@gmail.com>
2023-10-13 18:52:52 +0300

    bot update
    Added first button

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '6461556392:AAEECLgnBQWKCMoahhH2HEe4U5dFQme7yPQ';
+const mongoose = require('mongoose');
+const Plant = require('./database');
 
 const bot = new TelegramBot(token, { polling: true });
 
@@ -21,5 +23,47 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(chatId, welcomeMessage, keyboard);
 });
 
-// ... Решта коду
+bot.onText(/Додати рослину/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Введіть назву рослини:');
+
+    bot.once('text', (msg) => {
+        const plantData = { name: msg.text };
+        bot.sendMessage(chatId, 'Введіть біологічну назву рослини:');
+
+        bot.once('text', (msg) => {
+            plantData.scientificName = msg.text;
+            bot.sendMessage(chatId, 'Введіть періодичність поливання (в днях):');
+
+            bot.once('text', (msg) => {
+                plantData.wateringInterval = parseInt(msg.text);
+                bot.sendMessage(chatId, 'Введіть умови проростання:');
+
+                bot.once('text', (msg) => {
+                    plantData.germinationConditions = msg.text;
+                    bot.sendMessage(chatId, 'Введіть час останнього поливання (у форматі YYYY-MM-DD):');
+
+                    bot.once('text', (msg) => {
+                        plantData.lastWatered = new Date(msg.text);
+                        bot.sendMessage(chatId, 'Введіть посилання на фото рослини:');
+
+                        bot.once('text', (msg) => {
+                            plantData.photoURL = msg.text;
+
+                            // Збереження рослини в базу даних
+                            const newPlant = new Plant(plantData);
+                            newPlant.save()
+                                .then(() => {
+                                    bot.sendMessage(chatId, 'Рослину додано до бази даних.');
+                                })
+                                .catch((error) => {
+                                    bot.sendMessage(chatId, `Помилка: ${error}`);
+                                });
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
 
